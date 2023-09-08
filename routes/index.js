608755a40ff730c4a5079559ea836a62f41c5b43
LOBSTER10000 <ghh112034@gmail.com>
2023-09-08 17:16:28 +0900

    부트스트랩 기초 공사

--- routes/index.js
+++ routes/index.js
@@ -4,10 +4,9 @@ const controller = require('../controller/Cmain');
 
 //view단 부분
 router.get('/', controller.output.index); //메인 루트페이지 뷰
-router.get('/notice', controller.output.noticeDetail); // 공지사항 전체 글 리스트 불러오는 페이지
-router.get('/noticePost', controller.output.noticePost); // 공지사항 글쓰는 페이지 뷰
-router.get('/noticeDetail/:noticeNo', controller.output.noticeOne); // 공지사항 상세 페이지 뷰
-router.get('/noticeUpdate/:noticeNo', controller.output.noticeUpdate); //공지사항 업데이트 페이지 뷰
+router.get('/user/login', controller.output.userLogin);
+router.get('/user/register', controller.output.userRegister);
+
 //기능부분
 router.post('/noticePost', controller.input.noticePost);
 router.delete('/noticeDelete/:noticeNo', controller.input.noticeDelete);
