89a17b5453a5c26f0ae9e5b59068578957a3e348
hongminyeong <minyung1240@daum.net>
2023-09-12 19:50:24 +0900

    test:env_gitignore

--- public/js/user/login.js
+++ public/js/user/login.js
@@ -1,16 +1,5 @@
-//TODO id 기억하기
-// 아이디 저장
-//  $(window).on('load', function() {
-//     var cookieArr = document.cookie.split("; ");
-//     // 아이디 기억하기 쿠키가 존재하면 아이디 띄워주기!
-//     if (cookieArr.includes('id=remember')) {
-//         var id_input = document.getElementById('user');
-//         var user_id = document.getElementById('user_id').innerText;
-//         id_input.value = user_id;
+//TODO cookie,session -> 아이디 저장
 
-//         $("#userid_Remember").prop('checked', true);
-//     }
-// })
 async function loginAxios() {
   const form = document.forms['loginForm'];
   const result = await axios({
