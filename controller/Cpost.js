85bf109af879aa29841ccaf57f279061f1899056
chitty12 <cty12angel@naver.com>
2023-09-14 15:19:27 +0900

    fix/myLetter_rendering

--- controller/Cpost.js
+++ controller/Cpost.js
@@ -6,16 +6,14 @@ const output = {
   },
 
   showMyLetter: async (req, res) => {
-
     const userInfo = req.session.userInfo;
-    const id = userInfo.id;
+    const { id, userId, nickname } = userInfo;
+
     await MyLetter.create({
       id: id,
     });
 
-    res.send('myletter page 출력!');
-    // res.render('MyLetter');
-
+    res.render('letter/myletter', { nickname: nickname });
   },
 
   showPost: async (req, res) => {
