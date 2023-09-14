0c19be9fe2af79ca1862dac15f26b27135ddbfd7
ne0n9uy <2leebins@naver.com>
2023-09-14 23:57:01 +0900

    views1: conflict 피하기 위해 router develop 걸로 수정

--- routes/index.js
+++ routes/index.js
@@ -46,10 +46,6 @@ router.get('/notice', controller.output.noticeMain);
 router.get('/notice/post', controller.output.noticePost);
 router.get('/user/myPage', controller.output.myPage);
 router.get('/notice/update/:noticeNo', controller.output.noticeUpdate);
-router.get('/letter/friends', controllerLetter.output.friends);
-router.get('/letter/friendConfirm', controllerLetter.output.friendConfirm);
-router.get('/letter/myLetter', controllerLetter.output.myLetter);
-router.get('/letter/yourLetter', controllerLetter.output.yourLetter);
 router.get('/notice/update/:noticeNo', controller.output.noticeUpdate);
 
 //기능부분 (api)
