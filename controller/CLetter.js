0b370baf96952a3f8bacde62617a6956089786f0
ne0n9uy <2leebins@naver.com>
2023-09-18 21:14:24 +0900

    views1: 내 편지함일 때만 삭제 버튼 추가

--- controller/CLetter.js
+++ controller/CLetter.js
@@ -18,6 +18,8 @@ const output = {
       where: { id: id },
     });
     const lord = userData.map((user) => user.dataValues); //친구목록 주인
+    // console.log('주인', lord.id);
+    // console.log('로그인 여부', req.session.userInfo.id);
 
     //비로그인시 친구목록 접근권한없음
     if (req.session.userInfo !== undefined) {
@@ -38,13 +40,26 @@ const output = {
         id: profile.id,
       }));
 
-      res.render('letter/friends', {
-        lord: lord[0],
-        friend: req.session.friend,
-        isLogin: true,
-        session: req.session.userInfo,
-        friendData: friendData,
-      });
+      //로그인 했고 자기 페이지일 때
+      if (lord.id == req.session.userInfo.id) {
+        res.render('letter/friends', {
+          lord: lord[0],
+          friend: req.session.friend,
+          isLogin: true,
+          session: req.session.userInfo,
+          friendData: friendData,
+          isMine: true,
+        });
+      } else {
+        res.render('letter/friends', {
+          lord: lord[0],
+          friend: req.session.friend,
+          isLogin: true,
+          session: req.session.userInfo,
+          friendData: friendData,
+          isMine: false,
+        });
+      }
     } else {
       res.render('user/login', {
         session: req.session.userInfo,
@@ -102,15 +117,15 @@ const output = {
   icon: async (req, res) => {
     const id = req.params.id;
     const result = await User.findOne({
-      where : {
-        id : id,
-      }
-    })
+      where: {
+        id: id,
+      },
+    });
     console.log(result.nickname);
-    res.render('letter/icon', { 
+    res.render('letter/icon', {
       session: req.session.userInfo,
-      data : result, 
-  });
+      data: result,
+    });
   },
 };
 
