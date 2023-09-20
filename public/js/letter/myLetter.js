75c6745e4750645b414195caa3ab03ada4910e8d
ne0n9uy <2leebins@naver.com>
2023-09-20 14:01:33 +0900

    ciew1: css 보완 & 메인 resize event 추가

--- public/js/letter/myLetter.js
+++ public/js/letter/myLetter.js
@@ -23,21 +23,21 @@ function prevPage() {
         const data = res.data.postData;
         console.log('data prev', data);
         const designMap = {
-          1 : '/img/letterIcons/px_acorn.png',
-          2 : '/img/letterIcons/px_apple.png',
-          3 : '/img/letterIcons/px_apple2.png',
-          4 : '/img/letterIcons/px_coin.png',
-          5 : '/img/letterIcons/px_food.png',
-          6 : '/img/letterIcons/px_hedgehog.png',
-          7 : '/img/letterIcons/px_lApple.png',
-          8 : '/img/letterIcons/px_nuts.png',
-          9 : '/img/letterIcons/px_panda.png',
-          10 : '/img/letterIcons/px_pear.png',
-          11 : '/img/letterIcons/px_persimmon.png',
-          12 : '/img/letterIcons/px_pumpkin.png',
-          13 : '/img/letterIcons/px_rabbit.png',
-          14 : '/img/letterIcons/px_squirrel.png',
-          15 : '/img/letterIcons/px_tree.png',
+          1: '/img/letterIcons/px_acorn.png',
+          2: '/img/letterIcons/px_apple.png',
+          3: '/img/letterIcons/px_apple2.png',
+          4: '/img/letterIcons/px_coin.png',
+          5: '/img/letterIcons/px_food.png',
+          6: '/img/letterIcons/px_hedgehog.png',
+          7: '/img/letterIcons/px_lApple.png',
+          8: '/img/letterIcons/px_nuts.png',
+          9: '/img/letterIcons/px_panda.png',
+          10: '/img/letterIcons/px_pear.png',
+          11: '/img/letterIcons/px_persimmon.png',
+          12: '/img/letterIcons/px_pumpkin.png',
+          13: '/img/letterIcons/px_rabbit.png',
+          14: '/img/letterIcons/px_squirrel.png',
+          15: '/img/letterIcons/px_tree.png',
         };
         const startIndex = (curPage - 1) * letterCnt;
         console.log('start prev', startIndex); //5
@@ -51,14 +51,15 @@ function prevPage() {
           if (data[dataIndex]) {
             const designNumber = data[dataIndex].postDesign;
             const imagePath = designMap[designNumber];
-        
             if (imagePath) {
               letterImg[dataIndex].src = imagePath;
+              letterImg[dataIndex].style.display = 'block';
             } else {
-              letterImg[dataIndex].src = ''; 
+              letterImg[dataIndex].src = '';
             }
           } else {
-            letterImg[dataIndex].src = '';
+            // letterImg[dataIndex].src = '';
+            letterImg[dataIndex].style.display = 'none';
           }
         }
 
@@ -97,40 +98,40 @@ function nextPage() {
       console.log('data next', data);
       const startIndex = curPage * letterCnt;
       const designMap = {
-        1 : '/img/letterIcons/px_acorn.png',
-        2 : '/img/letterIcons/px_apple.png',
-        3 : '/img/letterIcons/px_apple2.png',
-        4 : '/img/letterIcons/px_coin.png',
-        5 : '/img/letterIcons/px_food.png',
-        6 : '/img/letterIcons/px_hedgehog.png',
-        7 : '/img/letterIcons/px_lApple.png',
-        8 : '/img/letterIcons/px_nuts.png',
-        9 : '/img/letterIcons/px_panda.png',
-        10 : '/img/letterIcons/px_pear.png',
-        11 : '/img/letterIcons/px_persimmon.png',
-        12 : '/img/letterIcons/px_pumpkin.png',
-        13 : '/img/letterIcons/px_rabbit.png',
-        14 : '/img/letterIcons/px_squirrel.png',
-        15 : '/img/letterIcons/px_tree.png',
+        1: '/img/letterIcons/px_acorn.png',
+        2: '/img/letterIcons/px_apple.png',
+        3: '/img/letterIcons/px_apple2.png',
+        4: '/img/letterIcons/px_coin.png',
+        5: '/img/letterIcons/px_food.png',
+        6: '/img/letterIcons/px_hedgehog.png',
+        7: '/img/letterIcons/px_lApple.png',
+        8: '/img/letterIcons/px_nuts.png',
+        9: '/img/letterIcons/px_panda.png',
+        10: '/img/letterIcons/px_pear.png',
+        11: '/img/letterIcons/px_persimmon.png',
+        12: '/img/letterIcons/px_pumpkin.png',
+        13: '/img/letterIcons/px_rabbit.png',
+        14: '/img/letterIcons/px_squirrel.png',
+        15: '/img/letterIcons/px_tree.png',
       };
       // step 1) 각자 다른 이미지 path 가져오기
       for (let i = 0; i < letterImg.length; i++) {
         const dataIndex = i;
         console.log('data design', data[dataIndex]);
-        
-        
 
         if (data[dataIndex]) {
           const designNumber = data[dataIndex].postDesign;
           const imagePath = designMap[designNumber];
-      
+
           if (imagePath) {
             letterImg[dataIndex].src = imagePath;
+            letterImg[dataIndex].style.display = 'block';
           } else {
-            letterImg[dataIndex].src = ''; 
+            letterImg[dataIndex].src = '';
           }
         } else {
-          letterImg[dataIndex].src = '';
+          // letterImg[dataIndex].src = '';
+          letterImg[dataIndex].style.display = 'none';
         }
       }
 
@@ -214,7 +215,7 @@ function updateLikes(id) {
 
 // 4. 친구 신청 날렸을 때
 const btnAddFriend = document.querySelector('#btnAddFriend');
-btnAddFriend.addEventListener('click', addFriend);
+// btnAddFriend.addEventListener('click', addFriend);
 const imgAddFriend = document.querySelector('#imgAddFriend');
 function addFriend() {
   if (imgAddFriend.src !== 'http://localhost:8000/img/header/check.png') {
