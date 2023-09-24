57c07f61ecb8798ca1f601289f8ebad1041edd9f
ne0n9uy <2leebins@naver.com>
2023-09-24 17:34:48 +0900

    view: guide 추가

--- public/js/guide.js
+++ public/js/guide.js
@@ -3,28 +3,34 @@ const guideDiv = document.querySelector('.guideDiv');
 const guideOne = document.querySelector('#guideOne');
 const guideTwo = document.querySelector('#guideTwo');
 const guideThree = document.querySelector('#guideThree');
+const guideOneBubble = document.querySelector('#guideBubbleOne');
+const guideTwoBubble = document.querySelector('#guideBubbleTwo');
+const guideThreeBubble = document.querySelector('#guideBubbleThree');
 
+// 1. 순서대로 가이드 보여주기
 guideTwo.style.visibility = 'hidden';
 guideThree.style.visibility = 'hidden';
 
-guideDiv.addEventListener('click', guide3)
-
 let touchCnt = 0;
-
-function guide1() {
+guideDiv.addEventListener('click', () => {
   touchCnt += 1;
   console.log('touchCnt', touchCnt);
-  guideOne.style.display = 'none';
-  guideTwo.style.visibility = 'visible';
-}
 
-const guide2= ()=>{
-    guide1();
-    guideTwo.style.visibility = 'hidden';
-    guideThree.style.visibility = 'visible';
-}
-const guide3()=> {
-    guide2()
-  guideThree.style.display = 'hidden';
-  guideDiv.style.display = 'none';
-}
+  switch (touchCnt) {
+    case 1:
+      console.log('switch', touchCnt);
+      guideOne.style.display = 'none';
+      guideTwo.style.visibility = 'visible';
+      break;
+
+    case 2:
+      guideTwo.style.visibility = 'hidden';
+      guideThree.style.visibility = 'visible';
+      break;
+
+    case 3:
+      guideThree.style.display = 'hidden';
+      guideDiv.style.display = 'none';
+      break;
+  }
+});
